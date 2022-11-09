c694edbdc3189930a79c6c97515258fa01fb7928
Julius Kreutz <julius@kreutz.dev>
2022-11-10 00:22:52 +0100

    bump version

--- xcb-util-cursor-sys/src/lib.rs
+++ xcb-util-cursor-sys/src/lib.rs
@@ -1,4 +1,5 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
+#![doc(html_root_url = "https://docs.rs/xcb-util-cursor-sys/")]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
